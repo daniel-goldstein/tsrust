8d1bf62a0c9604bc789b945767a82e55716c7329
Daniel Goldstein <danielgold95@gmail.com>
2020-08-26 10:28:45 -0400

    add TODO

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ impl Edge {
     }
 }
 
+// TODO Track node list
 // Currently just represents whether the given node is a sample.
 // If false, the Node is not a sample and is instead an inferred ancestor.
 type Node = bool;
