8268dcfb6671159b62463f9d0233ce39781a6df6
Daniel Goldstein <danielgold95@gmail.com>
2020-08-26 10:22:16 -0400

    add tree sequence builder for writing test cases

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ use std::vec::Vec;
 
 type NodeId = std::option::Option<usize>;
 
-const EPS: f64 = 1e-6;
-
 #[derive(PartialEq, Eq, Debug)]
 pub struct Edge {
     parent: usize,
@@ -28,7 +26,12 @@ impl PartialOrd for Edge {
 
 impl Edge {
     fn new(parent: usize, child: usize, left: u64, right: u64) -> Edge {
-        Edge { parent, child, left, right }
+        Edge {
+            parent,
+            child,
+            left,
+            right,
+        }
     }
 }
 
@@ -47,10 +50,11 @@ pub struct Tree {
 
 impl TreeSequence {
     fn new() -> Self {
-        TreeSequence { edges: Vec::new() }
+        TreeSequence { edges: vec![] }
     }
 
-    fn add_edge(&mut self, e: Edge) {
+    fn add_edge(&mut self, child: usize, parent: usize, left: u64, right: u64) {
+        let e = Edge::new(child, parent, left, right);
         match self.edges.binary_search(&e) {
             Ok(pos) => self.edges.insert(pos, e),
             Err(pos) => self.edges.insert(pos, e),
@@ -58,6 +62,67 @@ impl TreeSequence {
     }
 }
 
+// TODO Consider how this works for things that start as roots
+pub struct TreeSequenceBuilder {
+    ts: TreeSequence,
+    last_breakpoint: u64,
+    curr_edges: Vec<(usize, usize, u64)>, // (child, parent, left)
+}
+
+impl TreeSequenceBuilder {
+    fn new() -> Self {
+        TreeSequenceBuilder {
+            ts: TreeSequence::new(),
+            last_breakpoint: 0,
+            curr_edges: vec![],
+        }
+    }
+
+    fn insert(mut self, children: Vec<usize>, parent: usize) -> Self {
+        for c in children {
+            self.curr_edges.push((c, parent.clone(), self.last_breakpoint));
+        }
+        self
+    }
+
+    fn breakpoint(mut self, breakpoint: u64) -> Self {
+        // for (c, p) in &self.curr_edges {
+        //     self.ts.add_edge(*c, *p, self.curr_left, breakpoint.clone());
+        // }
+        self.last_breakpoint = breakpoint;
+        self
+    }
+
+    fn transplant(mut self, children: Vec<usize>, new_parent: Option<usize>) -> Self {
+        for c in children {
+            match self.curr_edges.iter().position(|(child, _, _)| *child == c) {
+                Some(index) => {
+                    let (child, old_parent, left) = self.curr_edges[index];
+                    self.curr_edges.remove(index);
+                    // The tracked edge now ends at last_breakpoint and we start
+                    // a new ongoing edge from there
+                    self.ts.add_edge(child, old_parent, left, self.last_breakpoint);
+                    if let Some(new_parent) = new_parent {
+                        self.curr_edges.push((child, new_parent, self.last_breakpoint));
+                    }
+                },
+                None => panic!("Can't move child node that does not yet exist"),
+            }
+        }
+
+        self
+    }
+
+    fn end(mut self, seq_length: u64) -> TreeSequence {
+        for (child, parent, left) in self.curr_edges {
+            self.ts.add_edge(child, parent, left, seq_length);
+        }
+
+        self.ts
+    }
+}
+
+// Trying to see if I can use a macro instead of the builder for fun
 // TODO: Support list of children -> parent and left/right
 #[macro_export]
 macro_rules! treeseq {
@@ -65,13 +130,12 @@ macro_rules! treeseq {
         {
             let mut ts = TreeSequence::new();
             $(
-                ts.add_edge(Edge::new($v, $u, 0, 1));
+                ts.add_edge($u, $v, 0, 1);
             )*
             ts
         }
     };
 }
-
 
 impl Tree {
     fn new(parent: Vec<NodeId>) -> Self {
@@ -118,7 +182,7 @@ impl Tree {
 
 #[cfg(test)]
 mod test {
-    use super::{Tree, TreeSequence, Edge};
+    use super::*;
 
     #[test]
     fn test_parent() {
@@ -153,9 +217,42 @@ mod test {
     #[test]
     fn test_tree_sequence_macro() {
         let mut ts = TreeSequence::new();
-        ts.add_edge(Edge::new(0, 1, 0, 1));
-        ts.add_edge(Edge::new(0, 2, 0, 1));
-
+        ts.add_edge(1, 0, 0, 1);
+        ts.add_edge(2, 0, 0, 1);
         assert_eq!(treeseq!(1 -> 0, 2 -> 0), ts);
+
+        let ts = TreeSequenceBuilder::new().insert(vec![1, 2], 0).end(1);
+        assert_eq!(treeseq!(1 -> 0, 2 -> 0), ts);
+    }
+
+    #[test]
+    fn test_tree_sequence_builder() {
+        let ts = TreeSequenceBuilder::new()
+            .insert(vec![0, 1], 4).insert(vec![2, 3], 5)
+            .insert(vec![4, 5], 6)
+            .breakpoint(1)
+            .transplant(vec![0], Some(6))
+            .transplant(vec![1], Some(5))
+            .breakpoint(2)
+            .transplant(vec![0, 5], None)
+            .end(3);
+
+        let mut hand_rolled = TreeSequence::new();
+        hand_rolled.add_edge(0, 4, 0, 1);
+        hand_rolled.add_edge(1, 4, 0, 1);
+        hand_rolled.add_edge(2, 5, 0, 3);
+        hand_rolled.add_edge(3, 5, 0, 3);
+        hand_rolled.add_edge(4, 6, 0, 3);
+        hand_rolled.add_edge(5, 6, 0, 2);
+
+        hand_rolled.add_edge(0, 6, 1, 2);
+        hand_rolled.add_edge(1, 5, 1, 3);
+
+        assert_eq!(ts, hand_rolled);
+    }
+
+    #[test]
+    fn test_tree_sequence_builder_empty() {
+        assert_eq!(TreeSequence::new(), TreeSequenceBuilder::new().end(1));
     }
 }
